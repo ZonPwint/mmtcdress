05eda387057e103a8d4fef8a5aa2156bdabffe04
Msaiblay <babykalay96Q@gmail.com>
2021-02-28 08:58:04 +0630

    msai

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\BackendController;
+use App\Http\Controllers\DressController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('/deshboard',BackendController::class);
+Route::resource('/dress',DressController::class);
+Route::resource('/order',OrderController::class);
+Route::resource('/user',UserController::class);
